b48cef687247b98a10d752b8d22e2db89342832a
kjeller <kallestralman@gmail.com>
2022-10-30 11:02:49 +0100

    Add option to run MM as either server or client

---
+++ README.md
@@ -0,0 +1,9 @@
+# Run MM using display output (default)
+
+`./start.sh`
+
+# Run MM as webserver
+
+`./start server`
+
+A web server should now be available on localhost:8080
--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-docker-compose --env-file ./secret/secret.env up -d
+#!/usr/bin/env bash
+
+# run as client by default
+if [ "$1" == "server" ]; then
+MODE=mmserver
+else
+MODE=mmclient
+fi
+
+echo "Starting Magic Mirror as $MODE"
+docker-compose -f docker-compose_$MODE.yml --env-file ./secret/secret.env up -d
